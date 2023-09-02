5ef3532387d112dfa07eb0e4122eba6303f63362
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-02 17:20:54 -0600

    added function to determine whether timeblock is past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,83 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-var today = dayjs();
-
-$("#currentDay").text(today.format("MMM D, YYYY"));
-
+//tells browser to load the html and css first
 $(document).ready(function() {
+ 
   
-  // Example: Change the text of an element
-  $('#myElement').text('DOM is ready!');
+//display current day
+var today = dayjs();
+  $("#currentDay").text(today.format("MMM D, YYYY [at] hh:mm:ss a"));
   
-  // Example: Add a click event handler to a button
-  $('#myButton').click(function() {
-    alert('Button clicked!');
-  });
-  
+  // assign saveBtn click listener for user input and time stamp
+  $('.saveBtn').on("click", function() {
+    //get nearby values
+    console.log(this);
 
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+    //take the change from the sibling in html descrition attribute
+    var text =$(this).siblings(".description").val();
+
+    //take the change from the parent html id attribute
+    var time =$(this).parent().attr("id");
+
+
+    //set item in local storage
+    localStorage.setItem(time, text);
+  });
 });
+
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+//create a function to determine if the time blocks are past, present, or future
+    function tense() {
+      //get current time
+      var currentTime = moment().hour();
+
+      //loop over time blocks
+      $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(blockTime, currentTime)
+
+        //check to see what tense we are in
+        if (blockTime < currentTime) {
+          $(this).addClass("past");
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+        } else if (blockTime === currentTime) {
+          $(this).removeClass("past");
+          $(this).addClass("future");
+          $(this).removeClass("present");
+        } else {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+
+          //re-run function
+          tense();
+        }
+      })
+
+
+    // if (calTime.isBefore(today)) {
+    //   timeBlock.addClass("past");
+    // } else if (projectDate.isSame(today)) {
+    //   timeBlock.addClass("present");
+    // } else if (projectDate.isAfter(today)) {
+    //   timeBlock.addClass("future")
+    // }
+
+    // console.log(tense);
+  };
+
+
+
+// saveBtn.addEventListener("click", userInput);
